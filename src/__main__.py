bec648ff1a471a177a91ef49892e106d51f70059
hungsean <hungsean0419@gmail.com>
2024-05-24 20:02:18 +0800

    完成最初始的模型

---
+++ src/__main__.py
@@ -0,0 +1,8 @@
+from body import body
+
+def main():
+    while True:
+        body()
+
+if __name__ == "__main__":
+    main()
---
+++ src/body.py
@@ -0,0 +1,6 @@
+
+def body() -> int:
+    print("> ", end="")
+    user_input = input()
+    print("what you input: ", user_input)
+    return 0
