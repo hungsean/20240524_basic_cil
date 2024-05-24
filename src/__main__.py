331a9846d36797ca017b7ddba201f818aab812ea
hungsean <hungsean0419@gmail.com>
2024-05-24 22:22:55 +0800

    完成基礎功能

--- src/__main__.py
+++ src/__main__.py
@@ -1,8 +1,12 @@
 from body import body
+from function import command
 
 def main():
     while True:
-        body()
+        body_code = body()
+        if body_code == 1:
+            break
+            
 
 if __name__ == "__main__":
     main()
