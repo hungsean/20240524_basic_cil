eb1623d8d7c1c3baa0d0af55cc6cad462f483070
hungsean <hungsean0419@gmail.com>
2024-07-22 13:56:14 +0800

    透過gpt_rebuild來更新一下這個地方

--- src/body.py
+++ src/body.py
@@ -1,20 +1,21 @@
-from function import command
+from function.command import exec_commands
 
 def body() -> int:
 
-    print("> ", end="")
+    print()
     try:
-        user_input = input("")
+        user_input = input("> ")
     except EOFError:
         # print("檢測到輸入結束（EOF）。程式將安全結束。")
         return 1# 可以在這裡返回或處理結束程式的其他邏輯
-    
-    user_input_split = user_input.split(' ', 1)
+    print()
+    # user_input_split = user_input.split(' ', 1)
     
     input_command = ""
     input_str_argument = ""
 
     # 如果分割後的 parts 長度是 2，說明有一個空格
+    user_input_split = user_input.split(maxsplit= 1)
     if len(user_input_split) == 2:
         input_command, input_str_argument = user_input_split
     else:
@@ -22,6 +23,9 @@ def body() -> int:
         input_command = user_input_split[0]
         input_str_argument = ""
 
-    command.exec_commands(input_command, input_str_argument)    
+    return_code = exec_commands(input_command, input_str_argument)    
+    if return_code == 1:
+        print("[body/body] exiting")
+        return 1
 
     return 0
--- src/function/command.py
+++ src/function/command.py
@@ -1,13 +1,26 @@
 from commands import test
 
+def menu(_):
+    for key in commands.keys():
+        print("- ", key)
+    return 0
+
+def exit_program(_):
+    print("程式即將結束...")
+    return 1
+
 commands = {
-    "test": test.index
+    "test": test.index,
+    "menu": menu,
+    "exit": exit_program,
 }
 
 def exec_commands(command_name: str, str_arguments: str):
     for key, value in commands.items():
         if key == command_name:
-            value(str_arguments)
+            return_code = value(str_arguments)
+            if return_code == 1:
+                return 1
             return 0
     print("[exec_commands] no command found")
     return -1
